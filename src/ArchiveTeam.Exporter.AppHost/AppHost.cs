fff9b5fbbd40fa753a20cd82de5cac73f5f9c4e6
Chris Meller <chris@doesnthaveone.com>
2026-04-20 00:52:02 -0400

    refactor(container): customize container image destination for registry
    Configure image push options to use registry endpoint and repository
    for the remote image name.

--- src/ArchiveTeam.Exporter.AppHost/AppHost.cs
+++ src/ArchiveTeam.Exporter.AppHost/AppHost.cs
@@ -12,11 +12,19 @@ var registry = builder.AddContainerRegistry("ghcr", registryEndpoint, registryRe
 #pragma warning restore ASPIRECOMPUTE003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 #pragma warning disable ASPIRECOMPUTE003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning disable ASPIREPIPELINES003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 var apiService = builder.AddProject<Projects.ArchiveTeam_Exporter_ApiService>("apiservice")
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
     .PublishAsDockerComposeService((resource, service) => { service.Name = "exporter"; })
+    .WithImagePushOptions(options =>
+    {
+        var endpointName = registryEndpoint.Resource.GetValueAsync(default).Result;
+        var repositoryName = registryRepository.Resource.GetValueAsync(default).Result;
+        options.Options.RemoteImageName = $"{endpointName}/{repositoryName}";
+    })
     .WithContainerRegistry(registry);
+#pragma warning restore ASPIREPIPELINES003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 #pragma warning restore ASPIRECOMPUTE003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 builder.Build().Run();
